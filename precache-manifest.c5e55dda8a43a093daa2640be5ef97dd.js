741389c0511ce6219877b2bd7ac18341cc40e007
yavgen999 <57105830+yavgen999@users.noreply.github.com>
2019-10-28 19:33:58 +0300

    Сервис

---
+++ precache-manifest.c5e55dda8a43a093daa2640be5ef97dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "750e5f014b0a5ecb38a3",
+    "url": "/static/js/main.750e5f01.chunk.js"
+  },
+  {
+    "revision": "b24a9e4e46d677c6c458",
+    "url": "/static/js/2.b24a9e4e.chunk.js"
+  },
+  {
+    "revision": "750e5f014b0a5ecb38a3",
+    "url": "/static/css/main.6704a9a5.chunk.css"
+  },
+  {
+    "revision": "b24a9e4e46d677c6c458",
+    "url": "/static/css/2.abc87da9.chunk.css"
+  },
+  {
+    "revision": "202f51a59f869e7ffd383b5150b0c800",
+    "url": "/index.html"
+  }
+];

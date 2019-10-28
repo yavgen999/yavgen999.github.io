9a6fe16610824ebeee18fbd13fb987eeb7c24e3b
yavgen999 <57105830+yavgen999@users.noreply.github.com>
2019-10-28 21:48:55 +0300

    Add files via upload

---
+++ precache-manifest.0ca8d6ce25845eee925661f27b11d06f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "77d7ea94c951c3cb3e01",
+    "url": "/static/js/main.77d7ea94.chunk.js"
+  },
+  {
+    "revision": "b24a9e4e46d677c6c458",
+    "url": "/static/js/2.b24a9e4e.chunk.js"
+  },
+  {
+    "revision": "77d7ea94c951c3cb3e01",
+    "url": "/static/css/main.6704a9a5.chunk.css"
+  },
+  {
+    "revision": "b24a9e4e46d677c6c458",
+    "url": "/static/css/2.abc87da9.chunk.css"
+  },
+  {
+    "revision": "e4aba159f84822c07d42ed5dd70209de",
+    "url": "/index.html"
+  }
+];

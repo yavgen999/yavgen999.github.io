5ad2c65d28f74b0f3ade1f91456d0ce4d5dae9e0
yavgen999 <57105830+yavgen999@users.noreply.github.com>
2019-12-09 20:39:07 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3014ff917fb6ce6f5f5bc688597c7412.js"
+  "/precache-manifest.6c640ef6c64014c6ac2a9d4939ecc00d.js"
 );
 
 workbox.clientsClaim();

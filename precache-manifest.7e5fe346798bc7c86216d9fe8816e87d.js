5c93102b729d3e9ae844dfe459837f7c45e3f9f3
yavgen999 <57105830+yavgen999@users.noreply.github.com>
2020-02-03 18:00:47 +0300

    Add files via upload

---
+++ precache-manifest.7e5fe346798bc7c86216d9fe8816e87d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "026664d6fe5f694e2235698e1a34efd1",
+    "url": "/static/media/mend.026664d6.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "22764f1afc8e34c4c2d0",
+    "url": "/static/js/main.ef14d41e.chunk.js"
+  },
+  {
+    "revision": "d7581d30a674c14a58b4",
+    "url": "/static/js/2.fc180584.chunk.js"
+  },
+  {
+    "revision": "22764f1afc8e34c4c2d0",
+    "url": "/static/css/main.9a982870.chunk.css"
+  },
+  {
+    "revision": "d7581d30a674c14a58b4",
+    "url": "/static/css/2.df3d2e14.chunk.css"
+  },
+  {
+    "revision": "0584a887107ab6f8a1c8c2004c2d05fd",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0cf5eb08241747e852632ac240e4ac53.js"
+  "/precache-manifest.7e5fe346798bc7c86216d9fe8816e87d.js"
 );
 
 workbox.clientsClaim();

d680ea42f6ca7d22c8e278589e2330d32ab2df43
yavgen999 <57105830+yavgen999@users.noreply.github.com>
2020-05-19 18:23:33 +0300

    Фикс спиннера и сепаратора да да да

---
+++ precache-manifest.a1513294c28093e67a098261981c2a9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "67777040c75421275b6c",
+    "url": "/static/js/main.080b51c8.chunk.js"
+  },
+  {
+    "revision": "687d6353041586c8458e",
+    "url": "/static/js/2.d7cff1d7.chunk.js"
+  },
+  {
+    "revision": "67777040c75421275b6c",
+    "url": "/static/css/main.dfa5011b.chunk.css"
+  },
+  {
+    "revision": "687d6353041586c8458e",
+    "url": "/static/css/2.3a41dc42.chunk.css"
+  },
+  {
+    "revision": "507cc0aeacadc602918e318e5a26104e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.723947919d6594aaa6c0204c345725d7.js"
+  "/precache-manifest.a1513294c28093e67a098261981c2a9f.js"
 );
 
 workbox.clientsClaim();

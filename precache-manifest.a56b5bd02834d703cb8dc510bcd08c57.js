2d62e011de91f2c1e98a826b3470af11f71f39c0
yavgen999 <57105830+yavgen999@users.noreply.github.com>
2020-05-23 18:12:43 +0300

    fix search history

---
+++ precache-manifest.a56b5bd02834d703cb8dc510bcd08c57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8a6a9976b561f17e5a5b",
+    "url": "/static/js/main.32e3d6eb.chunk.js"
+  },
+  {
+    "revision": "934f52f943d6e173a887",
+    "url": "/static/js/2.e1722646.chunk.js"
+  },
+  {
+    "revision": "8a6a9976b561f17e5a5b",
+    "url": "/static/css/main.dfa5011b.chunk.css"
+  },
+  {
+    "revision": "934f52f943d6e173a887",
+    "url": "/static/css/2.3a41dc42.chunk.css"
+  },
+  {
+    "revision": "65d97b8fed41c5622c8fc95ad92d9c5c",
+    "url": "/index.html"
+  }
+];

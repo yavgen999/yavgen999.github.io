b59d12425de3ae19c3288e2bfcc3ef97d3d12380
yavgen999 <57105830+yavgen999@users.noreply.github.com>
2020-07-29 19:28:56 +0300

    Add files via upload

---
+++ precache-manifest.7847fa302eeddc84b519a8af7d800c14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fad9ab039c3de5ab6b85f6fc87c85481",
+    "url": "/static/media/Montserrat-LightItalic.fad9ab03.otf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2a64a943d53f1836a947",
+    "url": "/static/js/main.18b59f8b.chunk.js"
+  },
+  {
+    "revision": "39636116ca4b71f44f44",
+    "url": "/static/js/2.1ab4b91a.chunk.js"
+  },
+  {
+    "revision": "2a64a943d53f1836a947",
+    "url": "/static/css/main.dfa5011b.chunk.css"
+  },
+  {
+    "revision": "39636116ca4b71f44f44",
+    "url": "/static/css/2.73421920.chunk.css"
+  },
+  {
+    "revision": "cd1540f7d52191ff78348b99c861d40d",
+    "url": "/index.html"
+  }
+];
